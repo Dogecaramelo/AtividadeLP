8b3bd270c86d9b7bc08130e1eae6ab5966878161
Dogecaramelo <harthurcp@gmail.com>
2024-12-06 23:22:18 -0300

    Update Curso.java

--- Curso.java
+++ Curso.java
@@ -1,17 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Curso {
     private String nome;
-    private String professor;
-    private Coordenador coordenador;
+    private List<Coordenador> coordenadores;
+    private List<Turma> turmas;
 
-    public Curso(String nome, String professor, Coordenador coordenador) {
+    public Curso(String nome) {
         this.nome = nome;
-        this.professor = professor;
-        this.coordenador = coordenador;
+        this.coordenadores = new ArrayList<>();
+        this.turmas = new ArrayList<>();
+    }
+
+    public void adicionarCoordenador(Coordenador coordenador) {
+        coordenadores.add(coordenador);
+    }
+
+    public void adicionarTurma(Turma turma) {
+        turmas.add(turma);
     }
 
     public void exibirInformacoesCurso() {
         System.out.println("Curso: " + nome);
-        System.out.println("Professor: " + professor);
-        System.out.println("Coordenador: " + coordenador.getNome());
+        System.out.println("Coordenadores:");
+        for (Coordenador coordenador : coordenadores) {
+            System.out.println(" - " + coordenador.getNome());
+        }
+        for (Turma turma : turmas) {
+            System.out.println("Turma: " + turma.getNome());
+            System.out.println("Professores:");
+            for (Professor professor : turma.getProfessores()) {
+                System.out.println(" - " + professor.getNome());
+            }
+            System.out.println("Alunos:");
+            for (Aluno aluno : turma.getAlunos()) {
+                System.out.println(" - " + aluno);
+            }
+        }
     }
 }
